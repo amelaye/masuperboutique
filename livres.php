aa92c41b4a08ee63cb8a7c8b5e19b673f5f04917
Amelie DUVERNET <amelieonline@gmail.com>
2019-07-08 18:01:28 +0200

    Ajout de champs et de photos.

--- livres.php
+++ livres.php
@@ -4,11 +4,12 @@ $livres = array(
         "titre" => "Le rouge et le Noir",
         "auteur" => "Stendhal",
         "prix" => 4.78,
-        "note" => 4,
+        "note" => 3,
         "genre" => "Classique",
         "collection" => "Folio",
         "langue" => "Français",
         "image" => "img/rougeetnoir.jpg",
+        "nb_pages" => 512,
         "resume" => "Fils de charpentier, Julien Sorel est trop sensible et trop ambitieux pour suivre la carrière 
         familiale dans la scierie d’une petite ville de province. En secret, il rêve d’une ascension similaire à 
         celle de Napoléon Bonaparte. Julien trouve une place de précepteur dans la maison du maire, Monsieur de Rénal, 
@@ -24,6 +25,8 @@ $livres = array(
         "collection" => "J'ai lu",
         "langue" => "Français",
         "image" => "img/tronedefer1.jpg",
+        "format" => "Poche",
+        "nb_pages" => 790,
         "resume" => "Le royaume des sept couronnes est sur le point de connaître son plus terrible hiver: par-delà 
         le mur qui garde sa frontière nord, une armée de ténèbres se lève, menaçant de tout détruire sur son passage. 
         Mais il en faut plus pour refroidir les ardeurs des rois, des reines, des chevaliers et des renégats qui se 
@@ -39,6 +42,8 @@ $livres = array(
         "collection" => "J'ai lu",
         "langue" => "Français",
         "image" => "img/tronedefer2.jpg",
+        "format" => "Poche",
+        "nb_pages" => 960,
         "resume" => "Le royaume des sept couronnes est sur le point de connaître son plus terrible hiver : par-delà 
         le mur qui garde sa frontière nord, une armée de ténèbres se lève, menaçant de tout détruire sur son passage. 
         Mais il en faut plus pour refroidir les ardeurs des rois, des reines, des chevaliers et des renégats qui se 
@@ -54,6 +59,8 @@ $livres = array(
         "collection" => "Points",
         "langue" => "Français",
         "image" => "img/journal.jpg",
+        "format" => "Broché",
+        "nb_pages" => 288,
         "resume" => "Le voyage en Inde d'un golden boy repenti en quête de sagesse. Avocat d'affaires à New York, 
         amateur de fêtes et de drogues..., Jonathan plaque tout du jour au lendemain pour partir à la recherche du 
         bonheur. Pendant 3 ans, il va à la rencontre de penseurs et de maîtres spirituels à travers le monde, et 
@@ -68,6 +75,8 @@ $livres = array(
         "collection" => "J'ai lu",
         "langue" => "Français",
         "image" => "img/pouvoir-moment.jpg",
+        "format" => "Poche",
+        "nb_pages" => 256,
         "resume" => "Ce livre nous présente plusieurs défis : si nous réussissons à être totalement présent et à faire 
         un pas à la fois dans le moment présent, si nous réussissons aussi à vraiment appréhender les réalités 
         que sont notre corps énergétique, le lâcher-prise, le pardon et le non-manifeste, c'est que nous saurons 
